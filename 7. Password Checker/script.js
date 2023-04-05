15a2374b1a336b61a7b58ae24092b23707911e85
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-04-05 17:15:11 +0530

    updated

--- 7. Password Checker/script.js
+++ 7. Password Checker/script.js
@@ -1,27 +1,30 @@
 class User {
+    #password;
     constructor(username, password) {
         this.username = username;
         this.#password = password;
     }
 
-    get password() {
+    getpassword() {
         return this.#password.replace(/./g, '*');
     }
 
-    set password(newPassword) {
-        if (newPassword.length < 8 || !/\\d/.test(newPassword) || !/[A-Z]/.test(newPassword)) {
+    setpassword(newPassword) {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        if (!passwordRegex.test(newPassword)) {
             console.error('Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter.');
         } else {
             this.#password = newPassword;
         }
     }
 
-    #password;
+
 }
 
 const user = new User('john', 'Password123');
+console.log(user.getpassword());
 
-user.password = 'newpassword';
-user.password = 'Newpassword';
-user.password = 'NewPassword12334343434';
-console.log(user.password);
+user.setpassword('Newpassword');
+user.setpassword('newpassword123');
+user.setpassword('Newassword12334343434');
+console.log(user.getpassword());
