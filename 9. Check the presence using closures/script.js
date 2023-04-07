6c6aa1f69381b814595287e79a02a06af9031ad5
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-04-07 20:03:28 +0530

    updated

--- 9. Check the presence using closures/script.js
+++ 9. Check the presence using closures/script.js
@@ -6,6 +6,6 @@ function numberChecker(arr) {
 
 let checktheNumber = new numberChecker([1, 2, 3, 4, 5, 8, 9])
 console.log(checktheNumber(3));
-console.log(checktheNumber(3, 5));
+console.log(checktheNumber(3, 5))
 console.log(checktheNumber(3 && 11))
 console.log(checktheNumber(10))
